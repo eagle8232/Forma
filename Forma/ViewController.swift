7813d2729c9ff99240c9220e2d00ece9220a271d
eagle8232 <zevs.1418@gmail.com>
2026-01-21 05:02:27 -0800

    Initial Commit

---
+++ Forma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Forma
+//
+//  Created by Vusal Nuriyev on 1/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
